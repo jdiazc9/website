9df8f59ef9275eb4f91aedf00f07a650aebf1845
jdiazc9 <juan.diazcolunga@gmail.com>
2020-01-25 21:04:05 +0100

    rebuilding site, Sat 01/25/2020

---
+++ update-website.bat
@@ -0,0 +1,21 @@
+@echo off
+
+cd C:\Users\Juan\Desktop\website
+
+echo "Making new website locally (calling Hugo)"
+C:\Users\Juan\AppData\Roaming\Hugo\hugo.exe -t academic
+
+echo "Updating base repository: jdiazc9/website.git"
+set msg="rebuilding site, %date%"
+git add .
+git commit -m %msg%
+git push origin master
+
+echo "Updating website repository: jdiazc9/jdiazc9.github.io.git"
+cd public
+git add .
+git commit -m %msg%
+git push origin master
+
+echo "Done"
+pause
